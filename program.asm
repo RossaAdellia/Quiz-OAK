8ff9ca8cfc3be7fe5d540f1d9535848f89090aa7
Rossa Adellia <09011382429126@student.unsri.ac.id>
2025-04-14 15:29:31 +0700

    Add files via upload

---
+++ program.asm
@@ -0,0 +1,7 @@
+and ketika 2 kondisi terpenuhi
+or salah satu terpenuhi
+salah satu terpenuhi adalah true atau benar
+kalau terpenuhi hasilnya false
+|| untuk or
+&& untuk and
+konversi tambahkan tipe data int yang di kurung yang ingin di tambah
